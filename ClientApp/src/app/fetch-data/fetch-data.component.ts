875567e58b97569141cd78a6034d99e8b5fe5f4e
yulivee <coding@voiding-warranties.de>
2019-04-14 17:48:34 +0200

    add ros bridge library

--- ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,6 +1,16 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+interface RobotJoints {
+  joint1: number;
+  joint2: number;
+  joint3: number;
+  joint4: number;
+  joint5: number;
+  joint6: number;
+  joint7: number;
+}
+
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
@@ -9,7 +19,11 @@ export class FetchDataComponent {
   public forecasts: WeatherForecast[];
 
   constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
+    const data = <RobotJoints> {
+      joint1: 1
+    };
+    
+    http.post<any>(baseUrl + 'api/SampleData/move',data).subscribe(result => {
       this.forecasts = result;
     }, error => console.error(error));
   }
--- Controllers/SampleDataController.cs
+++ Controllers/SampleDataController.cs
@@ -31,6 +31,23 @@ namespace scara_web_backend.Controllers
             });
         }
 
+        public class RobotJoints {
+            public int joint1 {get;set;}
+            public int joint2 {get;set;}
+            public int joint3 {get;set;}
+            public int joint4 {get;set;}
+            public int joint5 {get;set;}
+            public int joint6 {get;set;}
+            public int joint7 {get;set;}
+        }
+
+        [HttpPost("move")]
+        public void move(){
+        }
+        [HttpGet("status")]
+        public void status(){
+        }
+
         [HttpGet("test")]
         public string test() {
             var socket = new RosSocket(new WebSocketNetProtocol("ws://127.0.0.1:9090"));
