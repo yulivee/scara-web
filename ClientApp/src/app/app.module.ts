a1d2bc00ebbcd6b95f4344a3f7e9951ed4b24e91
Sandra Schuhmacher <coding@voiding-warranties.de>
2019-05-17 19:16:01 +0200

    material draft

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
@@ -13,6 +13,9 @@ import { RelativeDriveComponent } from './robot/rel-drive.component';
 import { RobotParametersComponent } from './robot/parameters.component';
 import { CustomCommandComponent } from './robot/commands.component';
 import { ProgrammingComponent } from './robot/programming.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatTabsModule, MatFormFieldModule, MatInputModule, MatSlideToggleModule, MatButtonModule, MatCardModule, MatGridListModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -26,7 +29,26 @@ import { ProgrammingComponent } from './robot/programming.component';
     AbsoluteDriveComponent,
     RelativeDriveComponent
   ],
+  exports: [
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatButtonModule,
+    MatCardModule,
+    MatGridListModule,
+  ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
